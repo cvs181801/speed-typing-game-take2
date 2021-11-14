819955e3d2d254f93e1b500ba67651db0a04decc
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-11-14 09:18:09 -0800

    add style

--- src/App.js
+++ src/App.js
@@ -41,8 +41,12 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>It's 1991 and you're on dial-up.</h1>  
-      <h2>Try to type the Hacker's Manifesto quote from the Mentor before your mom walks into the room!</h2>
+      <h1
+        className="header1"
+      >It's 1991 and you're on dial-up.</h1>  
+      <h2
+        className="header2"
+      >Try to type the Hacker's Manifesto quote from the Mentor before your mom walks into the room!</h2>
       
       <textarea
         style={{width: '50%', height: '20em'}}
@@ -53,30 +57,31 @@ export default function App() {
         disabled ={gameStart === false}
       />
       <h4>Seconds Remaining: {timeRemaining}</h4>
-      <button
+      <button 
+        className="button"
         onClick={handleGameStart}
         disabled={gameStart === true}
       >Start!</button>
       <h1>Word Count: {wordCount} </h1>
 
-      <p>This is our world now... the world of the electron and the switch, the
-beauty of the baud.  We make use of a service already existing without paying
-for what could be dirt-cheap if it wasn't run by profiteering gluttons, and
-you call us criminals.  We explore... and you call us criminals.  We seek
-after knowledge... and you call us criminals.  We exist without skin color,
-without nationality, without religious bias... and you call us criminals.
-You build atomic bombs, you wage wars, you murder, cheat, and lie to us
-and try to make us believe it's for our own good, yet we're the criminals.
+      <p className="manifesto">This is our world now... the world of the electron and the switch, the
+          beauty of the baud.  We make use of a service already existing without paying
+          for what could be dirt-cheap if it wasn't run by profiteering gluttons, and
+          you call us criminals.  We explore... and you call us criminals.  We seek
+          after knowledge... and you call us criminals.  We exist without skin color,
+          without nationality, without religious bias... and you call us criminals.
+          You build atomic bombs, you wage wars, you murder, cheat, and lie to us
+          and try to make us believe it's for our own good, yet we're the criminals.
 
-        Yes, I am a criminal.  My crime is that of curiosity.  My crime is
-that of judging people by what they say and think, not what they look like.
-My crime is that of outsmarting you, something that you will never forgive me
-for.
+                  Yes, I am a criminal.  My crime is that of curiosity.  My crime is
+          that of judging people by what they say and think, not what they look like.
+          My crime is that of outsmarting you, something that you will never forgive me
+          for.
 
-        I am a hacker, and this is my manifesto.  You may stop this individual,
-but you can't stop us all... after all, we're all alike.
+                  I am a hacker, and this is my manifesto.  You may stop this individual,
+          but you can't stop us all... after all, we're all alike.
 
-                               +++The Mentor+++</p>
+                                        +++The Mentor+++</p>
     </div>
   );
 }
